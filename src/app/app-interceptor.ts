81ce6ee9fb496bc0cf339106c61f5bf5f5e24f4b
mishostef <mishostef@abv.bg>
2020-02-19 15:25:38 +0200

    interceptor

---
+++ src/app/app-interceptor.ts
@@ -0,0 +1,17 @@
+import {Injectable} from '@angular/core';
+import {HttpInterceptor,HttpRequest, HttpHandler, HttpEvent} from '@angular/common/http'
+import {Observable} from 'rxjs';
+import {environment} from '../../src/environments/environment';
+
+
+const apiUrl = environment.apiUrl;
+
+@Injectable()
+export class AppInterceptor implements HttpInterceptor {
+  intercept(req:HttpRequest<any>, next: HttpHandler):Observable<HttpEvent<any>> {
+    const fullURL =req.url.includes('http')?req.url:`${apiUrl}${req.url}`;
+    const isApiRequest = fullURL.includes(apiUrl);
+    return next.handle(req.clone({url:fullURL, withCredentials: isApiRequest}));
+  }
+}
+
